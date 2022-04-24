74b6a100369ecc9fcb524d0842e8dacaac025a1f
yangacer <yangacer@gmail.com>
2022-04-24 21:32:27 +0800

    Support cmake --install.

--- include/hidx/encap.h
+++ include/hidx/encap.h
@@ -5,7 +5,8 @@
 #ifndef ENCAPSULATION_H_
 #define ENCAPSULATION_H_
 
-#define call(Ref, Mfn, ...) ((*(Ref).fnptr_->Mfn)((Ref).inst_, ##__VA_ARGS__))
+#define call(Ref, Mfn, ...) \
+  ((*(Ref).fnptr_->Mfn)((Ref).inst_ __VA_OPT__(,)  __VA_ARGS__))
 
 #define is_valid_ref(Ref) ((Ref).inst_ != 0 && (Ref).fnptr_ != 0)
 
