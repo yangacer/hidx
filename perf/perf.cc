99e11037a8c66731569900bca7f94c02b452a115
yangacer <yangacer@gmail.com>
2022-04-24 22:45:18 +0800

    Add perf tool.

---
+++ perf/perf.cc
@@ -0,0 +1,78 @@
+#include <chrono>
+#include <iostream>
+#include <random>
+#include <unordered_map>
+
+extern "C" {
+#include "hidx/encap.h"
+#include "hidx/hidx.h"
+}
+
+using TimePoint = std::chrono::steady_clock::time_point;
+struct ElapsedTimer {
+  auto elapsed() const {
+    auto now = TimePoint::clock::now();
+    return std::chrono::duration_cast<std::chrono::microseconds>(now - start);
+  }
+  void reset() { start = TimePoint::clock::now(); }
+  TimePoint start = TimePoint::clock::now();
+};
+
+int main(int argc, char**argv) {
+
+  size_t data_count = 1 << 20;
+  std::random_device rd;  //Will be used to obtain a seed for the random number engine
+  std::mt19937 gen(rd());  // Standard mersenne_twister_engine seeded with rd()
+  std::uniform_int_distribution<> distrib('a', 'z');
+
+  std::vector<std::string> data;
+  for (int n = 0; n < data_count; ++n) {
+    std::string str;
+    str.resize(distrib(gen));
+    for (auto &c : str)
+      c = (char)distrib(gen);
+
+    data.emplace_back(std::move(str));
+  }
+
+  {
+    std::cout << "[unordered set]\n";
+    ElapsedTimer timer;
+
+    std::unordered_map<std::string, std::string*> us;
+    us.reserve(data_count * 0.7);
+    for (auto& i : data)
+      us.emplace(i, &i);
+    std::cout << " insert=" << timer.elapsed().count() << std::endl;
+
+    timer.reset();
+    for (auto i : data) {
+      if(!us.count(i))
+        abort();
+    }
+    std::cout << " count=" << timer.elapsed().count() << std::endl;
+  }
+
+  {
+    std::cout << "[hidx]\n";
+    ElapsedTimer timer;
+
+    auto hi = create_hidx(data_count * 0.7, [](const void* val) -> key_desc_t {
+      auto *s = (std::string*)val;
+      return { s->c_str(), s->size() };
+    });
+
+    for (auto& i : data)
+      if(!call(hi, insert, &i))
+        abort();
+
+    std::cout << " insert=" << timer.elapsed().count() << std::endl;
+
+    timer.reset();
+    for (auto i : data)
+      call(hi, count, {i.c_str(), i.size()});
+    std::cout << " count=" << timer.elapsed().count() << std::endl;
+  }
+
+  return 0;
+}
