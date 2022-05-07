40210596c157a539d9da2dee5fc9dbb86ff237be
yangacer <yangacer@gmail.com>
2022-05-07 19:57:31 +0800

    Align bucket size with 16 bytes boundary.

--- tool/fibgen.py
+++ tool/fibgen.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 import sys
 
-table = [ 2, 3 ]
+table = [ 16, 24 ]
+until = int(sys.argv[1])
 
 while True:
     new = table[-1] + table[-2]
-    if new > int(sys.argv[1]):
+    new += new % 16
+    if new > until:
         break;
     table.append(new)
 
 print 'table size:', len(table)
 
-for i in range(0, len(table), 16):
-    row = ','.join([str(j) for j in table[i:i+16]])
-    print row
+print  ','.join([str(j) for j in table])
